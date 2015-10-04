aa348f8562226cdcb56cdfa6eed82a65ff08d4b8
Joshua Briley <josh@thebrileys.com>
2015-10-03 21:06:37 -0400

    Initial brainstorm for app functions.

--- js/app.js
+++ js/app.js
@@ -12,6 +12,55 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    // .new will call the newGame() function on click
+
+    var newGame = function(){
+      // New Game on page load
+    };
+
+
+    var randomNumber = function(){
+      // Generate a random number between 1 and 100
+      // return generated number
+    };
+
+    var accuracyFeedback = function(){
+      // compare user guess to random number
+      // if user number is higher than randomNumber
+        // return "Too High"
+        // else if user number is lower than randomNumber
+          // return "Too Low"
+        // else
+          // return "Correct Guess"
+    };
+
+    var temperatureFeedback = function(randomNumber, userGuess){
+      // Compare user guess to random number
+      // if the absolute value of randomNumber - userGuess is > 0
+        // if the absolute value of randomNumber - userGuess > 40 
+          // return "Ice Cold"
+          // else if the value is >=30 && <40 
+            // return "Cold"
+          // else if the value is >=20 && <30
+            // return "Warm"
+          // else if the value is >=10 && <20
+            // return "Hot"
+          // else
+          // return "Very Hot"
+    };
+
+    // Not sure if this is necessary
+    var numberOfGuesses = function(){
+      // How many guesses has the user made?
+      // Log each guess in an array
+    };
+
+    var userGuess = function(){
+      // on submit button click
+        // Validate number is number between 1 and 100
+        // return the value of the input field
+    }
+
 });
 
 

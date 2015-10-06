f0405feb599c1a7c4356ad7ebcdb6687a95d02df
Joshua Briley <josh@thebrileys.com>
2015-10-05 23:25:44 -0400

    Set random and get user guess.

--- js/app.js
+++ js/app.js
@@ -12,26 +12,38 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-    // .new will call the newGame() function on click
+    //---------------------------
+    // Utility Helpers
+    //---------------------------
+      var logThis = function(value) {
+        // Set a variable that allows console.log debugging to be 
+        // toggled on or of in the console.
+        var debug = true;
+        if (debug) {
+          console.log(value);
+        }
+      };
 
+      var formFocus = function(){
+        // set focus back to the input, for whatever reason.
+        $('#userGuess').val("").focus();
+      }
+
+    //---------------------------
+    // Game Functions
+    //---------------------------
     var newGame = function(){
       // New Game on page load
+      formFocus();
+      randomNumber();
+      userGuess();
+      //accuracyFeedback();
     };
-
 
     var randomNumber = function(){
       // Generate a random number between 1 and 100
-      // return generated number
-    };
-
-    var accuracyFeedback = function(){
-      // compare user guess to random number
-      // if user number is higher than randomNumber
-        // return "Too High"
-        // else if user number is lower than randomNumber
-          // return "Too Low"
-        // else
-          // return "Correct Guess"
+      var randomNumber = Math.floor(Math.random() * (100 -1) + 1);
+      logThis(randomNumber);
     };
 
     var temperatureFeedback = function(randomNumber, userGuess){
@@ -49,18 +61,44 @@ $(document).ready(function(){
           // return "Very Hot"
     };
 
-    // Not sure if this is necessary
-    var numberOfGuesses = function(){
-      // How many guesses has the user made?
-      // Log each guess in an array
-    };
-
     var userGuess = function(){
-      // on submit button click
-        // Validate number is number between 1 and 100
-        // return the value of the input field
+      var userGuessCount = 1;
+      $('#guessButton').on("click", function(e){
+        e.preventDefault();
+        var userGuess = $('#userGuess'),
+            userGuessValue = parseInt(userGuess.val());
+        logThis(userGuessCount);
+        if (userGuessValue > 0 && userGuessValue <= 100) {
+          $("<li>" + userGuessValue + "</li>").appendTo('#guessList');
+          $('#count').html(userGuessCount++);
+          formFocus(); 
+          return userGuessValue;
+        } else {
+          logThis("Please enter a number between 1 and 100");
+          alert("Sorry, that didn't meet the number requirements. Please enter a number between 1 and 100.");
+          formFocus();
+        }  
+      });
     }
 
+    var accuracyFeedback = function(){
+      // compare user guess to random number
+      logThis("random number: " + randomNumber() + " || user guess: " + userGuess());
+      // if user number is higher than randomNumber
+        // return "Too High"
+        // else if user number is lower than randomNumber
+          // return "Too Low"
+        // else
+          // return "Correct Guess"
+    };
+
+    //---------------------------
+    // Game Instantiation
+    //---------------------------
+      $('.new').on("click", function(){
+        newGame();
+      });
+      newGame();
 });
 
 
